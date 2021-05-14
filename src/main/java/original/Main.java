9cf810abd907fb3411ccc47e83a8571ec45c6c30
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2021-05-13 22:50:12 -0300

    criei a classe letras minusculas

--- src/main/java/original/Main.java
+++ src/main/java/original/Main.java
@@ -1,15 +1,21 @@
 package original;
 
+import com.sun.source.util.SourcePositions;
+
 public class Main {
 
     public static void main(String[] args) {
-        ResultadoDeAnaliseNumeroCaracteres resultadoDeAnaliseNumeroCaracteres = new ResultadoDeAnaliseNumeroCaracteres("jabuticaba");
+        ResultadoDeAnaliseNumeroCaracteres resultadoDeAnaliseNumeroCaracteres = new ResultadoDeAnaliseNumeroCaracteres("jabuTicaba");
         int bonusObtidoCaracteres = resultadoDeAnaliseNumeroCaracteres.obterBonus();
         System.out.println(bonusObtidoCaracteres);
 
         ResultadoDeAnaliseLetrasMaiusculas resultadoDeAnaliseLetrasMaiusculas = new ResultadoDeAnaliseLetrasMaiusculas("jabuTicaba");
         int bonusObtidoLetrasMaiusculas = resultadoDeAnaliseLetrasMaiusculas.obterBonus();
         System.out.println(bonusObtidoLetrasMaiusculas);
+
+        ResultadoDeAnaliseLetrasMinusculas resultadoDeAnaliseLetrasMinusculas = new ResultadoDeAnaliseLetrasMinusculas("jabuTicaba");
+        int bonusObtidoLetrasMinusculas = resultadoDeAnaliseLetrasMinusculas.obterBonus();
+        System.out.println(bonusObtidoLetrasMinusculas);
     }
 
 
