3f58388187456fe160731b8a70a4edc7aac2af2c
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2021-05-16 18:24:49 -0300

    Criei e conclui a classe Letras Sequenciais

--- src/main/java/original/Main.java
+++ src/main/java/original/Main.java
@@ -3,54 +3,57 @@ package original;
 public class Main {
 
     public static void main(String[] args) {
-        ResultadoDeAnaliseNumeroCaracteres resultadoDeAnaliseNumeroCaracteres = new ResultadoDeAnaliseNumeroCaracteres("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseNumeroCaracteres resultadoDeAnaliseNumeroCaracteres = new ResultadoDeAnaliseNumeroCaracteres("@@!!AAabcjabuTic4Ba");
         int bonusObtidoCaracteres = resultadoDeAnaliseNumeroCaracteres.obterBonus();
         System.out.println(bonusObtidoCaracteres);
 
-        ResultadoDeAnaliseLetrasMaiusculas resultadoDeAnaliseLetrasMaiusculas = new ResultadoDeAnaliseLetrasMaiusculas("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseLetrasMaiusculas resultadoDeAnaliseLetrasMaiusculas = new ResultadoDeAnaliseLetrasMaiusculas("@@!!AAabcjabuTic4Ba");
         int bonusObtidoLetrasMaiusculas = resultadoDeAnaliseLetrasMaiusculas.obterBonus();
         System.out.println(bonusObtidoLetrasMaiusculas);
 
-        ResultadoDeAnaliseLetrasMinusculas resultadoDeAnaliseLetrasMinusculas = new ResultadoDeAnaliseLetrasMinusculas("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseLetrasMinusculas resultadoDeAnaliseLetrasMinusculas = new ResultadoDeAnaliseLetrasMinusculas("@@!!AAabcjabuTic4Ba");
         int bonusObtidoLetrasMinusculas = resultadoDeAnaliseLetrasMinusculas.obterBonus();
         System.out.println(bonusObtidoLetrasMinusculas);
 
-        ResultadoDeAnaliseQuantidadeDeNumeros resultadoDeAnaliseQuantidadeDeNumeros = new ResultadoDeAnaliseQuantidadeDeNumeros("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseQuantidadeDeNumeros resultadoDeAnaliseQuantidadeDeNumeros = new ResultadoDeAnaliseQuantidadeDeNumeros("@@!!AAabcjabuTic4Ba");
         int bonusObtidoQuantidadeDeNumeros = resultadoDeAnaliseQuantidadeDeNumeros.obterBonus();
         System.out.println(bonusObtidoQuantidadeDeNumeros);
 
-        ResultadoDeAnaliseSimbolos resultadoDeAnaliseSimbolos = new ResultadoDeAnaliseSimbolos("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseSimbolos resultadoDeAnaliseSimbolos = new ResultadoDeAnaliseSimbolos("@@!!AAabcjabuTic4Ba");
         int bonusObtidoSimbolos = resultadoDeAnaliseSimbolos.obterBonus();
         System.out.println(bonusObtidoSimbolos);
 
-        ResultadoDeAnaliseNumerosSimbolosNoMeio resultadoDeAnaliseNumerosSimbolosNoMeio = new ResultadoDeAnaliseNumerosSimbolosNoMeio("@@!!AAajabuTic4Baa");
+        ResultadoDeAnaliseNumerosSimbolosNoMeio resultadoDeAnaliseNumerosSimbolosNoMeio = new ResultadoDeAnaliseNumerosSimbolosNoMeio("@@!!AAabcjabuTic4Ba");
         int bonusObtidoNumerosSimbolosMeio = resultadoDeAnaliseNumerosSimbolosNoMeio.obterBonus();
         System.out.println(bonusObtidoNumerosSimbolosMeio);
 
-        ResultadoDeAnaliseLetrasSomente resultadoDeAnaliseLetrasSomente = new ResultadoDeAnaliseLetrasSomente("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseLetrasSomente resultadoDeAnaliseLetrasSomente = new ResultadoDeAnaliseLetrasSomente("@@!!AAabcjabuTic4Ba");
         int bonusObtidoLetrasSomente = resultadoDeAnaliseLetrasSomente.obterBonus();
         System.out.println(bonusObtidoLetrasSomente);
 
-        ResultadoDeAnaliseRequisitosCumpridos resultadoDeAnaliseRequisitosCumpridos = new ResultadoDeAnaliseRequisitosCumpridos("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseRequisitosCumpridos resultadoDeAnaliseRequisitosCumpridos = new ResultadoDeAnaliseRequisitosCumpridos("@@!!AAabcjabuTic4Ba");
         int bonusObtidoRequisitosCumpridos = resultadoDeAnaliseRequisitosCumpridos.obterBonus();
         System.out.println(bonusObtidoRequisitosCumpridos);
 
-        ResultadoDeAnaliseNumerosSomente resultadoDeAnaliseNumerosSomente = new ResultadoDeAnaliseNumerosSomente("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseNumerosSomente resultadoDeAnaliseNumerosSomente = new ResultadoDeAnaliseNumerosSomente("@@!!AAabcjabuTic4Ba");
         int bonusObtidoNumerosSomente = resultadoDeAnaliseNumerosSomente.obterBonus();
         System.out.println(bonusObtidoNumerosSomente);
 
-        ResultadoDeAnaliseCaracteresConsecutivosInsensitive resultadoDeAnaliseCaracteresConsecutivosInsensitive = new ResultadoDeAnaliseCaracteresConsecutivosInsensitive("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseCaracteresConsecutivosInsensitive resultadoDeAnaliseCaracteresConsecutivosInsensitive = new ResultadoDeAnaliseCaracteresConsecutivosInsensitive("@@!!AAabcjabuTic4Ba");
         int bonusObtidoCaracteresConsecutivosInsensitive = resultadoDeAnaliseCaracteresConsecutivosInsensitive.obterBonus();
         System.out.println(bonusObtidoCaracteresConsecutivosInsensitive);
 
-        ResultadoDeAnaliseCaracteresConsecutivosMaiusculos resultadoDeAnaliseCaracteresConsecutivosMaiusculos = new ResultadoDeAnaliseCaracteresConsecutivosMaiusculos("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseCaracteresConsecutivosMaiusculos resultadoDeAnaliseCaracteresConsecutivosMaiusculos = new ResultadoDeAnaliseCaracteresConsecutivosMaiusculos("@@!!AAabcjabuTic4Ba");
         int bonusObtidoCaracteresConsecutivosMaisculos = resultadoDeAnaliseCaracteresConsecutivosMaiusculos.obterBonus();
         System.out.println(bonusObtidoCaracteresConsecutivosMaisculos);
 
-        ResultadoDeAnaliseCaracteresConsecutivosMinusculos resultadoDeAnaliseCaracteresConsecutivosMinusculos = new ResultadoDeAnaliseCaracteresConsecutivosMinusculos("@@!!AAajabuTic4Ba");
+        ResultadoDeAnaliseCaracteresConsecutivosMinusculos resultadoDeAnaliseCaracteresConsecutivosMinusculos = new ResultadoDeAnaliseCaracteresConsecutivosMinusculos("@@!!AAabcjabuTic4Ba");
         int bonusObtidoCaracteresConsecutivosMinusculos = resultadoDeAnaliseCaracteresConsecutivosMinusculos.obterBonus();
         System.out.println(bonusObtidoCaracteresConsecutivosMinusculos);
 
+        ResultadoDeAnaliseLetrasSequenciais resultadoDeAnaliseLetrasSequenciais = new ResultadoDeAnaliseLetrasSequenciais("@@!!AAabcjabuTic4Ba");
+        int bonusObtidoLetrasSequenciais = resultadoDeAnaliseLetrasSequenciais.obterBonus();
+        System.out.println(bonusObtidoLetrasSequenciais);
     }
 
 
